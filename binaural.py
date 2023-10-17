99cfd6518677695bd28959b70ce33ff5b52e8afb
TetraspaceW <holomanga@gmail.com>
2023-10-17 23:21:42 +0100

    dedupe stream setup

--- binaural.py
+++ binaural.py
@@ -9,16 +9,17 @@ volume = 0.5
 fs = 44100
 duration = 10.0
 
+base_frequency = 115.0
 # delta - 1-4 Hz
 # theta - 4-8 Hz
 # alpha - 8-14 Hz
 # beta - 14-30 Hz
 # gamma - 30-100 Hz
 # gamma / beta are where it gets interesting
-beat_frequency = 30
+beat_frequency = 30.0
 
-f_left = 100.0
-f_right = 100.0 + beat_frequency
+f_left = base_frequency - beat_frequency / 2.0
+f_right = base_frequency + beat_frequency / 2.0
 
 
 def sin(freq):
@@ -31,16 +32,14 @@ samples_left = sin(f_left)
 samples_right = sin(f_right)
 
 if not placebo:
-    samples_stereo = np.array(
-        [samples_left, samples_right]
-    ).T  # Combine left and right channels
-    output_bytes = (volume * samples_stereo).tobytes()
-    stream = p.open(format=pyaudio.paFloat32, channels=2, rate=fs, output=True)
+    samples = np.array([samples_left, samples_right]).T
 else:
-    samples_stereo = np.array(samples_left + samples_right) * 0.5
-    output_bytes = (volume * samples_stereo).tobytes()
-    stream = p.open(format=pyaudio.paFloat32, channels=1, rate=fs, output=True)
+    samples = np.array(samples_left + samples_right) * 0.5
 
+output_bytes = (volume * samples).tobytes()
+stream = p.open(
+    format=pyaudio.paFloat32, channels=2 if not placebo else 1, rate=fs, output=True
+)
 
 start_time = time.time()
 stream.write(output_bytes)
